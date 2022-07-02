cdebb32f6c082ffbf8fb4a4b0c59dff093d1e995
Darshita Gupta <darshitagupta@gmail.com>
2022-07-02 09:08:54 +0530

    my website

---
+++ project.js
@@ -0,0 +1,59 @@
+var email=document.forms['form']['email'];
+var password=document.forms['form']['password'];
+
+var email_error=document.getElementById('email_error');
+var pass_error=document.getElementById('pass_error');
+
+email.addEventListener('textInput',email_Verify);
+password.addEventListener('textInput',pass_Verify);
+
+function validated()
+{
+    if(email.value.length < 9)
+    {
+        email.style.border="1px solid red";
+        email_error.style.display="block";
+        email.focus();
+        return false;
+    }
+    if(email.value[0]=='@' || email.value[0]=='.')
+    {
+        window.alert("please enter a valid email");
+        email.focus();
+        return false;
+    }
+    if(email.value[0]>=0 && email.value[0]<=9)
+    {
+        if(isNaN(email.value))
+        {
+            window.alert("please enter valid phone number");
+            email.focus();
+            return false;
+        }
+    }
+    if(password.value.length < 6)
+    {
+        password.style.border="1px solid red";
+        pass_error.style.display="block";
+        password.focus();
+        return false;
+    }
+}
+
+function email_Verify(){
+    if(email.value.length >= 8)
+    {
+        email.style.border="1px solid silver";
+        email_error.style.display="none";
+        return true;
+    }
+}
+
+function pass_Verify(){
+    if(password.value.length >= 8)
+    {
+        password.style.border="1px solid silver";
+        pass_error.style.display="none";
+        return true;
+    }
+}
